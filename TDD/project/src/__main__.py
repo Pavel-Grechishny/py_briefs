fd0750f84e6f94aa81ebef7e8635773ba33c1f6e
Pavel-Grechishny <grechishny.p@mail.ru>
2023-12-16 19:20:10 +0300

    обзор: from .vars import *

---
+++ TDD/project/src/__main__.py
@@ -0,0 +1,39 @@
+"""
+Точка входа.
+"""
+
+
+
+
+
+
+
+
+
+
+# D:\temp\briefs\TDD\project\src
+# 19:01:37 > python -i __main__.py
+# >>>
+# >>> import utils
+# >>>
+# >>> utils
+# <module 'utils' from 'D:\\temp\\briefs\\TDD\\project\\src\\utils\\__init__.py'>
+# >>>
+# >>> type(utils)
+# <class 'module'>v
+
+# НЕВОЗМОЖНАЯ ОПЕРАЦИЯ
+# >>> utils.vars
+# ...
+# AttributeError: module 'utils' has no attribute 'vars'
+# >>>
+
+# ВОЗМОЖНАЯ ОПЕРАЦИЯ
+# >>> from utils import vars
+# >>>
+# >>> vars
+# <module 'utils.vars' from 'D:\\temp\\briefs\\TDD\\project\\src\\utils\\vars.py'>
+
+# ВОЗМОЖНАЯ ОПЕРАЦИЯ
+# >>> import utils.vars
+
--- TTD/project/readme.txt
+++
@@ -1,1 +0,0 @@
-project.toml - ConfigFile
